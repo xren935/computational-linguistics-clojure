b50a37c88a83b0b9600d8ff11b92695d7393944a
xren935 <xingya.ren@mail.mcgill.ca>
2020-11-01 20:19:19 +0100

    assign4 in progress

--- tempCodeRunnerFile.clj
+++ tempCodeRunnerFile.clj
@@ -1,1 +1,3 @@
-(println (lookup-probability "same" moby-vocab moby-word-probabilities))
+(defn theta-corpus-joint [theta corpus theta-probs]
+  (+ (score-corpus corpus theta)
+     (log2 (nth theta-probs (index-in-list theta thetas 0)))))
